fc9fc3959ff06a5b4ed9c066d4d05e3c4316e5e5
ArtemPushPop <artemiygotovtsev@gmail.com>
2021-10-30 12:25:32 +0300

    Модифицировал стресс тест, но все еще требует ревью.

--- tests/stress-test/stress_test.c
+++ tests/stress-test/stress_test.c
@@ -1,63 +1,60 @@
+#include "data_path.h"
+#include "TimeItExecl.h"
+#include "ExeclArgsCreater.h"
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "data_path.h"
+#include <unistd.h>
 
 
-
-int MakeCommand(char *cmd, size_t cmd_len, char *path, char *letters){
-    cmd[cmd_len] = '\0';
-    strcat(cmd, " -f ");
-    strcat(cmd, path);
-    strcat(cmd, " -p ");
-    strcat(cmd, letters);
-    return 0;
-}
-
-
-int MakePath(char *path, const char* dir, const char *file_name){
-    path[strlen(dir)] = '\0';
-    strcat(path, dir);
-    strcat(path, file_name);
-    return 0;
-}
-
-int main(){
-    char cmd_consistent[256];
-    strcpy(cmd_consistent, "./consistent_test");
-    size_t consistent_len = strlen(cmd_consistent);
-
-    char cmd_parallel[256];
-    strcpy(cmd_parallel, "./parallel_test");
-    size_t parallel_len = strlen(cmd_parallel);
+int main() {
+    char command[2][256];
+    size_t command_len[2];
+    strcpy(command[0], "./consistent_test");
+    command_len[0] = strlen(command[0]);
+    strcpy(command[1], "./parallel_test");
+    command_len[1] = strlen(command[1]);
 
     char files_path[256];
     char file_name[] = "file1.txt";
 
-    FILE *results[2][FILES_NUM];
-    results[1][1] = 0;
+    char results[256];
+    size_t s_results[2][3];
 
-    for (int i = 0; i < FILES_NUM; i++){
-        file_name[4] = '1' + (char)i;
-        MakePath(files_path, DATA_FILES_PATH, file_name);
-        MakeCommand(cmd_consistent, consistent_len, files_path, "ab");
-        if ((results[0][i] = popen(cmd_consistent, "r")) == NULL)
-            exit(1);
-        MakeCommand(cmd_parallel, parallel_len, files_path, "ab");
-        if ((results[1][i] = popen(cmd_parallel, "r")) == NULL)
-            exit(1);
-    }
-
-    size_t res1, res2;
-    for (int i = 0; i < FILES_NUM; i++){
-        fscanf(results[0][i], "%lu", &res1);
-        pclose(results[0][i]);
-        fscanf(results[1][i], "%lu", &res2);
-        pclose(results[1][i]);
-        if (res1 != res2)
+    for (int i = 0; i < FILES_NUM; i++) {
+        for (int j = 0; j < 2; j++) {
+            file_name[4] = '1' + (char) i;
+            MakePath(files_path, DATA_FILES_PATH, file_name);
+            MakeCommand(command[j], command_len[j], files_path, "ab");
+            TimeItExecl(command[j], results);
+//            sscanf(results, "%lu %lu", s_results[j] + 0, s_results[j] + 1, s_results[j] + 2);
+            sscanf(results, "%lu %lu", s_results[j] + 0, s_results[j] + 1);
+        }
+        if (s_results[0][0] != s_results[1][0])
             printf("RESULTS MISMATCH! FROM FILE: file%d.txt\n", i + 1);
         else
             printf("RESULTS MATCH! FROM FILE: file%d.txt\n", i + 1);
+//        printf("TIME EXECUTED CONSISTENT FILE (file%d.txt): sec - %lu, microsec - %lu \n", i + 1, s_results[0][1], s_results[0][2]);
+//        printf("TIME EXECUTED PARALLEL FILE (file%d.txt): sec - %lu, microsec - %lu \n", i + 1, s_results[1][1], s_results[1][2]);
+        printf("TIME EXECUTED CONSISTENT FILE (file%d.txt): tacts - %lu \n", i + 1, s_results[0][1]);
+        printf("TIME EXECUTED PARALLEL FILE (file%d.txt): tacts - %lu\n", i + 1, s_results[1][1]);
+        printf("\n");
     }
+
     return 0;
+
+//    size_t res1, res2;
+//    for (int i = 0; i < FILES_NUM; i++) {
+//        fscanf(results[0][i], "%lu", &res1);
+//        pclose(results[0][i]);
+//        fscanf(results[1][i], "%lu", &res2);
+//        pclose(results[1][i]);
+//        if (res1 != res2)
+//            printf("RESULTS MISMATCH! FROM FILE: file%d.txt\n", i + 1);
+//        else
+//            printf("RESULTS MATCH! FROM FILE: file%d.txt\n", i + 1);
+//    }
+//    return 0;
 }
